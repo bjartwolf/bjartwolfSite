d650a0bc6c4ddf56423708a70fb60defb52b56fe
bjartwolf <bjartnes@gmail.com>
2013-03-16 15:58:24 +0100

    redirect to blog

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ app.configure(function () {
   app.use('/public', express.static(__dirname + '/public'));
   app.set('views', __dirname + '/template');
 });
+
 app.get('/', function (req, res) {
     res.render('index.jade');
+});
+app.get('/blog', function (req, res) {
+    res.redirect('http://bjartwolfblog.azurewebsites.net');
 });
 app.listen(process.env.port || 8000);
